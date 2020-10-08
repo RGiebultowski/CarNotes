0b70de9d8164f7c6e71c9e161c2c6d50b326a547
RGiebultowski <raf.giebultowsksi@gmail.com>
2020-10-08 14:34:15 +0200

    recycler View

--- app/src/main/java/Activity/GasTankUp.java
+++ app/src/main/java/Activity/GasTankUp.java
@@ -2,12 +2,15 @@ package Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 
 
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,9 +19,11 @@ import com.example.courseapp1.R;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
-public class GasTankUp extends AppCompatActivity {
+public class GasTankUp extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     public static final String NOWE_TANKOWANIE = "Nowe Tankowanie";
 
@@ -32,6 +37,10 @@ public class GasTankUp extends AppCompatActivity {
     private DateFormat dateFormat;
 
     private TextView milageTextView;
+    private TextView costTextView;
+    private TextView litersTextView;
+
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +59,27 @@ public class GasTankUp extends AppCompatActivity {
         dateEditText = findViewById(R.id.date);
         milageEditText = findViewById(R.id.milage);
         milageTextView = findViewById(R.id.milageTextView);
+        litersTextView = findViewById(R.id.litersTextView);
+        costTextView = findViewById(R.id.costTextView);
         litersEditText = findViewById(R.id.liters);
         costEditText = findViewById(R.id.cost);
         confirmTankButton = findViewById(R.id.confirmTankButton);
 
         dateEditText.setText(getCurrentDate());
+
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(context, GasTankUp.this, year, month, day);
+
+                datePickerDialog.show();
+            }
+        });
 
         confirmTankButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,12 +101,63 @@ public class GasTankUp extends AppCompatActivity {
             }
         });
 
+        costEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus == false){
+                    validateCosts();
+                }
+            }
+        });
+
+        litersEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus == false){
+                    validateLiters();
+                }
+            }
+        });
+
+    }
+
+    private boolean validateLiters() {
+        if (TextUtils.isEmpty(litersEditText.getText().toString())){
+            litersTextView.setText("Litry muszą zostać podane!");
+            litersTextView.setTextColor(getResources().getColor(R.color.red));
+            return false;
+        }else {
+            litersTextView.setText("Zatankowane Litry");
+            litersTextView.setTextColor(getResources().getColor(R.color.black));
+            return true;
+        }
+    }
+
+    private boolean validateCosts() {
+        if (TextUtils.isEmpty(costEditText.getText().toString())){
+            costTextView.setText("Koszty muszą zostać podane!");
+            costTextView.setTextColor(getResources().getColor(R.color.red));
+            return false;
+        }else {
+            costTextView.setText("Koszty Paliwa");
+            costTextView.setTextColor(getResources().getColor(R.color.black));
+            return true;
+        }
     }
 
     private boolean validateMileage() {
         if (TextUtils.isEmpty(milageEditText.getText().toString())){
             milageTextView.setText("Przebieg musi zostać podany!");
             milageTextView.setTextColor(getResources().getColor(R.color.red));
+            return false;
+        }
+        if (Integer.valueOf(milageEditText.getText().toString()) <= 0){
+            milageTextView.setText("Przebieg musi być wiekszy niz 0!");
+            milageTextView.setTextColor(getResources().getColor(R.color.red));
+            return false;
+        }else{
+            milageTextView.setText("Przebieg");
+            milageTextView.setTextColor(getResources().getColor(R.color.black));
         }
         int size = autoData.getTankUpRecord().size();
         if (autoData.getTankUpRecord().size() !=0){
@@ -128,5 +204,11 @@ public class GasTankUp extends AppCompatActivity {
         dateFormat = DateFormat.getDateInstance();
         Date date = new Date();
         return dateFormat.format(date);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = new GregorianCalendar(year, month, dayOfMonth);
+        dateEditText.setText(dateFormat.format(calendar.getTime()));
     }
 }
